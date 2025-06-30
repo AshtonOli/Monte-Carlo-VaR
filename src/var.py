a95cf3003bf7cacb07cca6936775fb31d221e709
AshtonOli <l0gz27xf@duck.com>
2025-06-30 16:28:09 +1000

    prettify var subplot

--- src/var.py
+++ src/var.py
@@ -8,11 +8,50 @@ def component_var():
     pass
 
 def produce_var_results(price_paths: pd.DataFrame, returns: pd.DataFrame,save_path: Optional[str] = None):
+    
     final_values = price_paths.iloc[-1].values
     losses = sorted(returns.close.iloc[-1] - final_values)
     var_res = [np.percentile(losses,i) for i in range(1,101)]
 
-    fig = go.Figure()
+    max_price_col = np.argmax(final_values == max(final_values))
+    min_price_col = np.argmax(final_values == min(final_values))
+
+    max_price_path = price_paths[f"sim_{1 + max_price_col}"]
+    min_price_path = price_paths[f"sim_{1 + min_price_col}"]
+
+    fig = make_subplots(rows = 1, cols = 2, subplot_titles=("Sample of price paths", "VaR Curve"))
+
+    fig.add_trace(
+        go.Scatter(
+            x = price_paths.index,
+            y = max_price_path,
+            name = "Max price path"
+        ),
+        row = 1,
+        col = 1
+    )
+    
+    fig.add_trace(
+        go.Scatter(
+            x = price_paths.index,
+            y = min_price_path,
+            name = "Min price path"
+        ),
+        row = 1,
+        col = 1
+    )
+
+    for i in (1 + (np.random.random(3)*(len(price_paths.columns)))):
+        fig.add_trace(
+            go.Scatter(
+                x = price_paths.index,
+                y = price_paths[f"sim_{int(i)}"],
+                showlegend=False
+            ),
+            row = 1,
+            col = 1
+        )
+
     fig.add_trace(
         go.Scatter(
             x = np.arange(1,101),
@@ -20,8 +59,11 @@ def produce_var_results(price_paths: pd.DataFrame, returns: pd.DataFrame,save_pa
             mode = "lines",
             line = {
                 "color" : "black"
-            }
-        )
+            },
+            name = "VaR Curve"
+        ),
+        row = 1,
+        col = 2,
     )
     fig.add_vline(
         x = 5,
@@ -29,11 +71,13 @@ def produce_var_results(price_paths: pd.DataFrame, returns: pd.DataFrame,save_pa
         line_color = "red",
         label = {
             "text" : "P95 Losses",
-            "textposition" : "top left"
-        }
+            "textposition" : "top right"
+        },
+        row = 1,
+        col = 2,
     )
     fig.update_layout(
-        title_text = f"VaR Curve\nP95 losses ${dollar_format(var_res[4])}"
+        title_text = f"VaR Curve:<br>P95 losses {dollar_format(var_res[4])}"
     )
 
     if save_path:
